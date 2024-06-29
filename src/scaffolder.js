7aef73ffbad5470ecc08bda2bfa951514f83956b
Matt Travi <programmer@travi.org>
2024-06-29 03:35:17 -0500

    feat(output): log info message to inform of scaffolding step

--- src/scaffolder.js
+++ src/scaffolder.js
@@ -1,8 +1,11 @@
 import {promises as fs} from 'node:fs';
 import touch from 'touch';
 import simpleGit from 'simple-git';
+import {info} from '@travi/cli-messages';
 
 export default async function ({projectRoot}) {
+  info('Initializing Git Repository');
+
   const git = simpleGit({baseDir: projectRoot});
 
   await Promise.all([
