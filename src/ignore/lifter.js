b79cd5dc2ecc81f39c5de297e8683d533f5b9dc8
Matt Travi <programmer@travi.org>
2024-06-30 09:10:36 -0500

    docs(output): logged when updating ignore file

--- src/ignore/lifter.js
+++ src/ignore/lifter.js
@@ -1,7 +1,13 @@
+import {info} from '@travi/cli-messages';
+
 import writeIgnores from './writer.js';
 
 export default async function ({projectRoot, results: {vcsIgnore}}) {
-  if (vcsIgnore) await writeIgnores({projectRoot, ...vcsIgnore});
+  if (vcsIgnore) {
+    info('Updating files and directories to be ignored from version control', {level: 'secondary'});
+
+    await writeIgnores({projectRoot, ...vcsIgnore});
+  }
 
   return {};
 }
