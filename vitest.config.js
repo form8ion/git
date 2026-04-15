76aa693b0faab7f7714066aca9c91411b8f9c677
Matt Travi <programmer@travi.org>
2026-04-14 21:12:09 -0500

    test(unit): modernize coverage collection

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,13 @@ import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
   test: {
-    restoreMocks: true
+    restoreMocks: true,
+
+    coverage: {
+      provider: 'v8',
+      reporter: ['lcov', 'text-summary', 'html'],
+      include: ['src/**'],
+      exclude: ['src/**/index.js']
+    }
   }
 });
